45b381aee74b1f9de7288fad8d68fc3a7ea8107b
aidan-stritch <aidan.stritch@gmail.com>
2020-04-25 16:34:06 +0000

    added settings for db

--- env.py
+++ env.py
@@ -1,3 +1,5 @@
 import os
 
 os.environ.setdefault("SECRET_KEY", "'-azoo!iklb9+27axe=zp8-s+%=th&@m1s(vi3m-z=shaup8*2!'")
+
+os.environ.setdefault("DATABASE_URL", "postgres://vjbtwghsndobuh:9bd69cd896704931b8c53364b8fb6f848cd3903f9e5f43c04d5ab9f7b42ebf58@ec2-54-217-204-34.eu-west-1.compute.amazonaws.com:5432/dfbo55h4f4ba20")
